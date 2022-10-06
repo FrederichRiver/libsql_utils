e713e31de0c4cc6fba7b7d88d9acc087815f5686
FrederichRiver <friederich@163.com>
2022-10-06 09:07:22 +0800

    v1.2.5 修改了formStock的定义方式，以解决在merge过程中发生的StaleDataError问题

--- libsql_utils/model/stock.py
+++ libsql_utils/model/stock.py
@@ -4,28 +4,52 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+def get_formStock(tablename):
+    Base = declarative_base()
+    class formStock(Base):
+        __tablename__ = tablename
+        trade_date = Column(Date, primary_key=True)
+        stock_name = Column(String(20))
+        close_price = Column(Float, default=0)
+        high_price = Column(Float, default=0)
+        low_price = Column(Float, default=0)
+        open_price = Column(Float, default=0)
+        prev_close_price = Column(Float, default=0)
+        change_rate = Column(Float, default=0)
+        amplitude = Column(Float, default=0)
+        volume = Column(Float, default=0)
+        turnover = Column(Float, default=0)
+        adjust_factor = Column(Float, default=1)
+                
+        def __str__(self):
+            return f"<Stock {self.__tablename__}>"
+    return formStock
 
-class formStock(Base):
-    __tablename__ = 'template_stock'
-    trade_date = Column(Date, primary_key=True)
-    stock_name = Column(String(20))
-    close_price = Column(Float, default=0)
-    high_price = Column(Float, default=0)
-    low_price = Column(Float, default=0)
-    open_price = Column(Float, default=0)
-    prev_close_price = Column(Float, default=0)
-    change_rate = Column(Float, default=0)
-    amplitude = Column(Float, default=0)
-    volume = Column(Integer, default=0)
-    turnover = Column(Float, default=0)
-    adjust_factor = Column(Float, default=1)
-
-    def __str__(self):
-        return "<Stock template>"
+# class formStock(Base):
+#     __tablename__ = 'template_stock'
+#     trade_date = Column(Date, primary_key=True)
+#     stock_name = Column(String(20))
+#     close_price = Column(Float, default=0)
+#     high_price = Column(Float, default=0)
+#     low_price = Column(Float, default=0)
+#     open_price = Column(Float, default=0)
+#     prev_close_price = Column(Float, default=0)
+#     change_rate = Column(Float, default=0)
+#     amplitude = Column(Float, default=0)
+#     volume = Column(Integer, default=0)
+#     turnover = Column(Float, default=0)
+#     adjust_factor = Column(Float, default=1)
+#     __mapper_args__ = {
+#         'version_id_col':trade_date,
+#         'version_id_generator': False
+#     }
+#     def __str__(self):
+#         return "<Stock template>"
 
 class formStockManager(Base):
     __tablename__ = 'stock_manager'
     stock_code = Column(String(10), primary_key=True)
+    stock_code2 = Column(String(10))
     stock_name = Column(String(20))
     orgId = Column(String(25))
     short_code = Column(String(10))
